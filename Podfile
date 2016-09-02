c02de702df2c1a6a00f439ca64def98ed75860b6
daeun9205 <daeun_k@naver.com>
2016-09-02 18:34:35 +0900

    afnetworking 1st upload

---
+++ Podfile
@@ -0,0 +1,6 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘8.1’
+
+target “afnetworking_test” do
+	pod 'AFNetworking', '~> 2.5.4’
+end
--- afnetworking_test/ViewController.m
+++ afnetworking_test/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
 
@@ -14,14 +15,68 @@
 
 @implementation ViewController
 
+@synthesize results;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://api.themoviedb.org/3/movie/now_playing?api_key=d74a7e1423e9267f335de909f5a25f84"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDataTask *dataTask = [manager dataTaskWithRequest:request completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@", error);
+        } else {
+            //NSLog(@"%@ %@", response, responseObject);
+            
+            NSError *error = nil;
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:responseObject options:0 error:&error];
+            NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:nil];
+            
+            results = [dic objectForKey:@"results"];
+            
+            for (id dic in results)
+            {
+                //NSLog(@"poster_path = %@", [dic objectForKey:@"poster_path"]);
+                NSLog(@"title = %@", [dic objectForKey:@"title"]);
+            }
+      
+        }
+    }];
+    
+    [dataTask resume];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [results count];
+}
+
+- (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: SimpleTableIdentifier];
+    if(cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SimpleTableIdentifier];
+    }
+    
+    NSDictionary *dic = [results objectAtIndex:indexPath.row];
+    cell.textLabel.text = [dic objectForKey:@"title"];
+    return cell;
+
+}
+
 
 @end
